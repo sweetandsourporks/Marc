2d4082ad48cf7989efc8b441b3b59c089968f712
Marc <granadamarc96@gmail.com>
2025-01-12 18:14:32 +0800

    Update and rename create_admins_table.py to database_editing_template.py

--- database_editing_template.py
+++ database_editing_template.py
@@ -6,13 +6,20 @@ connection = sqlite3.connect('admins.db')
 #cursor
 c = connection.cursor()
 
-#create a table
-c.execute("""CREATE TABLE admins (
-        email text,
-        first_name text,
-        last_name text,
-        contact_number text
-    )""")
+
+
+
+
+
+
+#place execute files here
+
+
+
+
+
+
+
 
 
 #commit the command
